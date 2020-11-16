3aed40bc01800b06d43b3e9e0e5a31318522c208
Cyroxin <cyroxin@github.com>
2020-11-16 20:56:24 +0200

    Add console log

--- production.js
+++ production.js
@@ -5,13 +5,13 @@ require('dotenv').config();
 
 module.exports = (app) => {
     app.enable('trust proxy');
-
     
     app.use ( (req, res, next) => {
         if (req.secure) {
             // request was via https, so do no special handling
             next();
         } else {
+            console.log("Redirecting to https");
             const proxypath = process.env.PROXY_PASS || ''
             // request was via http, so redirect to https
             res.redirect(301, `https://${req.headers.host}${proxypath}${req.url}`);
