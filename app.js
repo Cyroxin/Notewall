c60d7b2bde1d5665e2ce969b0e3548d5ac10661a
Cyroxin <cyroxin@github.com>
2020-11-24 16:52:22 +0200

    Create scalable structure for development

--- app.js
+++ app.js
@@ -1,50 +1,73 @@
 'use strict';
 
 require('dotenv').config();
+
+/* env file:
+DB_HOST=[host]
+DB_USER=[user]
+DB_PASS=[pass]
+DB_NAME=[name]
+production=false
+*/
+
+// On production, env file can also have line: "PROXY_PASS=/[proxy_pass]",
+// which adds the following addition to the path "http://ip/[proxy_pass]/index.html"
+
 const production = process.env.production || 'false'; // Is running on server? "true"/"false"
+const port = 3000;
+
+console.log("production: ", production);
 
 /* REQUIRE */
-//const express = require('express');
 const express = require('express');
+const app = express();
 
-// const route = require('./routes/nameRoute'); // Example route
+//const cats = require('./routes/postRoute');
+//const users = require('./routes/userRoute');
+//const auth = require('./routes/authRoute');
+
+//var passport = require('./utils/pass');
+
+if (!production) {
+  const cors = require('cors')
+  app.use(cors());
+}
 
 /* INIT */
 
-const app = express();
+app.use(express.json());
+//app.use(passport.initialize());
+
+app.use(express.static('uploads'));
+app.use(express.static('public_html'));
+app.use('/thumbnails', express.static('thumbnails'));
 
 
 /* CONFIGURE */
 
 
 /* ROUTE */
-//app.use('/nameOfRouteFolder', route); // EXAMPLE
 
-// TODO: Implement routing in separate file
+//app.use('/cat', passport.authenticate('jwt', { session: false }), cats);
+//app.use('/user', passport.authenticate('jwt', { session: false }), users);
+//app.use('/auth', auth);
 
-
-/* RUN */
-
-if (production === "true") {
-  console.log("production");
-  require('./production')(app, 3000);
-} else {
-  console.log("development");
-  require('./localhost')(app, 8000, 3000);
-}
-
-
-app.use(express.static('uploads'));
-
-
-app.get('/', (req, res) => // Example
+app.get('/test', (req, res) => // Example
 {
   res.send('Hello Secure World!');
 });
 
 
-app.get('/test', function (req, res) {
-  res.send('hello world')
-})
+/* RUN */
 
-//module.export = production; // Is running on server? "true"/"false"
+
+if (production) {
+  require('./require/servers/production')(app, port);
+} else {
+  require('./require/servers/localhost')(app, 8000, port);
+}
+
+
+module.export = production; // Is running on server? "true"/"false"
+
+/*END*/ 
--- require/servers/localhost.js
+++ require/servers/localhost.js
@@ -8,6 +8,9 @@ const sslkey = fs.readFileSync('ssl-key.pem');
 const sslcert = fs.readFileSync('ssl-cert.pem');
 const options = { key: sslkey, cert: sslcert };
 
+// If an error is hit here, please ensure you have your locally signed key & certificate for https support.
+
+
 const httpsRedirect = (req,res)  => {
     res.writeHead(301, { 'Location': 'https://localhost:8000' + req.url });
       res.end();
